0feab157ae1315a983691313c458c894cd1f1dad
Administrator <Administrator@APL-5CD128FLCS>
2022-04-05 01:03:52 +0530

    Add project files.

---
+++ GITHUBAPP/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GITHUBAPP.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
